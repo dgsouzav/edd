1de69b2f85e80137a41b05d18bbf40d433df2a90
Diogo Souza <diogouba11@gmail.com>
2022-03-30 19:55:22 -0300

    Update ex4.c

---
+++ lista3/ex4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int n = 1, 9, i;  
+    
+    for (i = 1;i <= 5; i++)
+    { 
+        n = n + 3;
+        printf("%d ",n);
+    }
+    
+    for (i = 9;i <= 5; i++)
+    { 
+        n = n + 3;
+        printf("%d ",n);
+    }
+    
+    return 0;
+
+}
