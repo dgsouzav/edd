47bd5eaf29f22ffa7e6cd10e4bc4f174846d8f03
Diogo Souza <diogouba11@gmail.com>
2022-05-08 10:21:37 -0300

    update ex1

---
+++ lista7/ex1.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    float numero1, numero2
+    
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &numero1);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &numero2);
+    
+}
+
